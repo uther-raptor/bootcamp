9ce9d79a02451f9e25d195e18d6bfb4258b685fb
Unknown <utheraptor@gmail.com>
2018-06-27 18:37:22 +0200

    Jsem největší umělec

---
+++ p5-boilerplate/empty-example/sketch.js
@@ -0,0 +1,13 @@
+// do not remove, allowes autocomplete
+/// <reference path="./../p5.global-mode.d.ts" />
+
+function setup() {
+  createCanvas(400, 400);
+  fill("pink");
+  rect(10, 10, 100, 100);
+  // put setup code here
+}
+
+function draw() {
+  // put drawing code here
+}
