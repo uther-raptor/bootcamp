a0972652272386154cfc6ce2c574a64c7808b875
Unknown <utheraptor@gmail.com>
2018-06-29 11:42:56 +0200

    playing with colors

--- hrajeme-si-s-tweety/sketch.js
+++ hrajeme-si-s-tweety/sketch.js
@@ -2,9 +2,9 @@
 /// <reference path="./../p5.global-mode.d.ts" />
 
 function setup() {
-  // put setup code here
-  createCanvas(400, 400);
-  rect(10, 10, 100, 100);
+  var vysledek = tweets[0].text;
+
+  console.log(vysledek);
 }
 
 function draw() {
--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,13 +1,48 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
 
+
+var clicks = 10;
+var keys = 10;
+var time = 0;
+var timespeed = 1;
+var myHue = 0;
+
 function setup() {
-  createCanvas(400, 400);
-  fill("pink");
-  rect(10, 10, 100, 100);
-  // put setup code here
+  createCanvas(4000, 4000);
+  angleMode(DEGREES);
+  colorMode(HSB, 100);
 }
 
-function draw() {
-  // put drawing code here
+function draw() { 
+  time = time + timespeed;  
+  var ellipseOffset = time % 4000;
+  var myHue = time % 50;
+
+if (ellipseOffset > 2000) {
+background("red");
+} else {
+background(myHue, 100, 100);
 }
+
+
+  if (keyIsPressed) {
+    timespeed = 5;
+  } else {
+    timespeed = 1;
+  }
+
+  ///stroke("pink");
+  fill("black");
+
+  ellipse(ellipseOffset, 200, clicks * 10, clicks * 10);
+  ellipse(300, 300, clicks * 10, clicks * 10);
+}
+
+function mouseReleased() {
+  clicks = clicks + 1;
+}
+
+function keyReleased() {
+  keys = keys + 1;
+} 
